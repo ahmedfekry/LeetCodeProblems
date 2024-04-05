e13d9e14328e6a9e9a96934fd4f9843ffdecd1a3
Ahmed Fekry <ahmedfikry78@gmail.com>
2024-04-05 22:50:34 +0200

    solved greatest element on the right problem

---
+++ InPlaceArrayOperations/GreatestElementOnRight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.InPlaceArrayOperations
+{
+    public class GreatestElementOnRight
+    {
+
+        /*
+         * 
+         * https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3259/
+         * Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+            After doing so, return the array.
+
+ 
+         */
+        public int[] ReplaceElements(int[] arr)
+        {
+            int Max = arr[arr.Length-1];
+            arr[arr.Length - 1] = -1;
+            for (int i = arr.Length-2;i >= 0; i--)
+            {
+                int temp = arr[i];
+                arr[i] = Max;
+                Max = Math.Max(Max, temp);
+            }
+            return arr;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LeetCodeProblems
+﻿using LeetCodeProblems.InPlaceArrayOperations;
+
+namespace LeetCodeProblems
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GreatestElementOnRight greatestElement = new GreatestElementOnRight();
+
+            int[] arr = { 400 };
+            arr = greatestElement.ReplaceElements(arr);
+
+            printArray(arr);
+        }
+
+        public static void printArray(int[] arra)
+        {
+            Console.Write("[");
+            foreach (int i in arra)
+            {
+                Console.Write(i);
+                Console.Write(",");
+            }
+            Console.Write("]");
         }
     }
 }
