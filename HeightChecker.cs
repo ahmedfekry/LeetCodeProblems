f9a8740eb02a630e06639aba8298d1d1c0d60547
Ahmed Fekry <ahmedfikry78@gmail.com>
2024-04-14 03:33:11 +0200

    added the height checker problem

---
+++ HeightChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class HeightChecker
+    {
+        public int HeightCheckerSol(int[] heights)
+        {
+            int[] expected = new int[heights.Length];
+
+            Array.Copy(heights, expected,heights.Length);
+
+            Array.Sort(expected);
+            int result = 0;
+            for (int i = 0;i < expected.Length;i++)
+            {
+                if (expected[i] != heights[i])
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
